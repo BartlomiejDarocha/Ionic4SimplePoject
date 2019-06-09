4bc9d6d1c0a51f8f41918b55869a72b1969f4be6
Bartlap <bartlomiejdarocha@gmail.com>
2019-06-09 18:53:36 +0200

    walidacja trening

--- IonicSimpleProject/src/app/second-page/second-page.page.ts
+++ IonicSimpleProject/src/app/second-page/second-page.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TestService } from '../services/test.service';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-second-page',
@@ -11,7 +12,10 @@ export class SecondPagePage implements OnInit {
   param1: any;
   param2: any;
   names: Array<string>;
-  constructor(private activatedRoute: ActivatedRoute, private testService: TestService) { }
+
+  form2: FormGroup;
+
+  constructor(private activatedRoute: ActivatedRoute, private testService: TestService, private formBuilder:FormBuilder) { }
 
   ngOnInit() {
     this.param1 = this.activatedRoute.snapshot.paramMap.get('param1');
@@ -20,6 +24,25 @@ export class SecondPagePage implements OnInit {
       this.names = data;
     });
     console.log(this.names, ' names');
+    this.form2 = this.formBuilder.group({
+      nickname: new FormControl('', Validators.compose([
+        Validators.maxLength(14),
+        Validators.minLength(5),
+        Validators.pattern('^[0-9]{1,3}[a-z]{5,10}$'),
+        Validators.required,
+      ]))
+    });
+  }
+  onSubmit2(){
+    console.log('submit!');
+  }
+  form2Erorrs = {
+    nickname: [
+      { type:'maxlength', message: 'za dlugie'},
+      { type:'minlength', message: 'za krotkie'},
+      { type:'pattern', message: 'zly wzór'},
+      { type:'required', message: 'wymagane'},
+    ]
   }
 
 }
