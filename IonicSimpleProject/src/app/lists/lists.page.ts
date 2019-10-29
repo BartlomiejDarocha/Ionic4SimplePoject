d225bad46ea037867137a1c6506b212852e1ccf3
BartlomiejDarocha <bartlomiejdarocha@gmail.com>
2019-10-29 22:19:11 +0100

    ViweChild train

--- IonicSimpleProject/src/app/lists/lists.page.ts
+++ IonicSimpleProject/src/app/lists/lists.page.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.page.html',
   styleUrls: ['./lists.page.scss'],
 })
-export class ListsPage implements OnInit {
+export class ListsPage implements AfterViewInit {
   @ViewChild('content') private content: any;
   @ViewChild('section') private section: ElementRef;
+  @ViewChild('divTest') private divTest: ElementRef;
   public activeCard = 0;
   public position = 0;
   constructor(
   ) { }
 
-  ngOnInit() {
-    //console.log(this.content, 'content');
+  // ngOnInit() {
+  //   //console.log(this.content, 'content');
+    
+  // }
+  ngAfterViewInit(): void {
     this.position = document.getElementById('test0').offsetTop;
+    this.divTest.nativeElement.innerHTML = 'Hello Angular 7!';
+    console.log(this.divTest.nativeElement, 'divTest.nativeElement');
+    this.divTest.nativeElement.style.color = 'red';
+
   }
   public activeCardHandler(i: number) {
     let test2;
