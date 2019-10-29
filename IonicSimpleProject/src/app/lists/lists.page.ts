7c6e4bc126d8e292c76439ddaa1a8888b84ff813
BartlomiejDarocha <bartlomiejdarocha@gmail.com>
2019-10-29 22:05:25 +0100

    upAndDown slide in component on eleREf and documnet selector

--- IonicSimpleProject/src/app/lists/lists.page.ts
+++ IonicSimpleProject/src/app/lists/lists.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-lists',
@@ -7,18 +7,31 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class ListsPage implements OnInit {
   @ViewChild('content') private content: any;
-
+  @ViewChild('section') private section: ElementRef;
   public activeCard = 0;
-  constructor() { }
+  public position = 0;
+  constructor(
+  ) { }
 
   ngOnInit() {
-    console.log(this.content);
+    //console.log(this.content, 'content');
+    this.position = document.getElementById('test0').offsetTop;
   }
   public activeCardHandler(i: number) {
+    let test2;
     this.activeCard = i;
+    test2 = document.getElementById(`test${this.activeCard}`).offsetTop;
+    this.position = test2;
+    console.log(test2, 'test2');
   }
   public srcollToBottom() {
     console.log('scorll executed');
     this.content.scrollToBottom(300);
+  }
+  public checkRefernce() {
+    console.log(this.section, 'section');
+    console.log(this.section.nativeElement.classList, 'native elment');
+  }
+  public ElRefenceTest() {
   }
 }
